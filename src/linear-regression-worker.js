5ea476b076534f1a5b607d4d951805b08c628a99
Julia Camille Menchavez <jcmenchavez@gmail.com>
2014-10-19 12:27:01 +0800

    implemented update theta method

--- src/linear-regression-worker.js
+++ src/linear-regression-worker.js
@@ -29,6 +29,10 @@ function train(data) {
     training_data = data;
     var hypothesis_cost = cost();
     send('log', hypothesis_cost);
+    for (var i = 0; i < thetas.length; i++) {
+        updated = update(i);
+        send('log', 'updated theta ' + i + ': ' + updated);
+    }
     // @todo solve for the cost given the values of the thetas
     // @todo update the thetas if necessary
     //       everytime the thetas array is fully updated, send a message
@@ -55,8 +59,16 @@ function cost() {
 /*
  * Computes the new value for the given theta and returns it.
  */
-function update(theta) {
-
+function update(index) {
+    var result = 0;
+    for (var i = 0; i < training_data.length; i++) {
+       var sum = thetas[0];
+       for (var j = 0; j < training_data[i].features.length; j++) {
+            sum += (thetas[j + 1] * training_data[i].features[j] )
+       }
+       result += (sum - training_data[i].label) * (index === 0 ? 1 : training_data[i].features[index - 1]);
+    }
+    return thetas[index] - ((alpha / training_data.length) *  result);
 }
 
 function send(command, data) {
